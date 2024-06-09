2c59ced0de4d9719cfc0d89d876e222e5b3a17ad
hushuhan <873933169@qq.com>
2024-06-09 17:56:50 +0800

    main: Added 添加功能函数，获得彩色小波图和灰度小波图

--- src/core/Encoder.py
+++ src/core/Encoder.py
@@ -12,6 +12,7 @@ from PIL import Image
 
 import src.core.utils as utils
 
+
 class Encoder(ABC):
     """
     Encoder 抽象基类
@@ -19,18 +20,20 @@ class Encoder(ABC):
     @abstractmethod
     def encode(self, image_path, save_path):
         raise NotImplementedError("抽象类中不进行实现: Encoder.encode()!")
-    
+
     def read(self, file_path):
         raise NotImplementedError("抽象类中不进行实现: Encoder.read()!")
 
     def save(self, file_path, bitstream):
         raise NotImplementedError("抽象类中不进行实现: Encoder.save()!")
-    
+
+
 class JP2KEncoder(Encoder):
     """
     JPEG2000 编码器
     """
-    def __init__(self, q_factor: int=5, tile_size: int=128) -> None:
+
+    def __init__(self, q_factor: int = 5, tile_size: int = 128) -> None:
         self.q_factor = q_factor
         # TODO: 后面可以改成自适应图片（求300以内的 w 和 h 的最大公因数）
         self.tile_size = tile_size
@@ -43,13 +46,31 @@ class JP2KEncoder(Encoder):
         tiles = JP2KEncoder.tile_np_image(img, self.tile_size)
         bitstream, _ = JP2KEncoder.organize_bitstream(tiles, self.q_factor)
         return bitstream
-    
+
     def read(self, img_path: str) -> np.ndarray:
         return cv2.imread(img_path)
 
     def save(self, save_path: str, bitstream: ByteString) -> None:
         with open(save_path, 'wb') as f:
             f.write(bitstream)
+
+    def get_wavelet_image(self, img: np.ndarray) -> Tuple:
+        lt_0, (rt_0, lb_0, rb_0) = JP2KEncoder.discrete_wavelet_transform_2d(
+            img[:, :, 0])
+        lt_1, (rt_1, lb_1, rb_1) = JP2KEncoder.discrete_wavelet_transform_2d(
+            img[:, :, 1])
+        lt_2, (rt_2, lb_2, rb_2) = JP2KEncoder.discrete_wavelet_transform_2d(
+            img[:, :, 2])
+        lt = np.stack([lt_0, lt_1, lt_2], axis=2)
+        rt = np.stack([rt_0, rt_1, rt_2], axis=2)
+        lb = np.stack([lb_0, lb_1, lb_2], axis=2)
+        rb = np.stack([rb_0, rb_1, rb_2], axis=2)
+        return lt, rt, lb, rb
+
+    def get_gray_wavelet_image(self, img: np.ndarray) -> Tuple:
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        lt, (rt, lb, rb) = JP2KEncoder.discrete_wavelet_transform_2d(gray_img)
+        return lt, rt, lb, rb
 
     @staticmethod
     def tile_np_image(image, tile_size) -> List[np.ndarray]:
@@ -61,19 +82,19 @@ class JP2KEncoder(Encoder):
         """
         h, w = image.shape[:2]
         tiles = []
-        
+
         if isinstance(tile_size, int):
             tile_size = utils.turn2pair(tile_size)
         tile_h, tile_w = tile_size
-        
+
         # 遍历图像并提取块
         for x in range(0, w, tile_w):
             for y in range(0, h, tile_h):
                 tile = image[y:y+tile_h, x:x+tile_w]
                 tiles.append(tile)
-        
+
         return tiles
-    
+
     @staticmethod
     def convert_color2YUV(image: Union[np.ndarray, Image.Image]) -> np.ndarray:
         """
@@ -87,7 +108,7 @@ class JP2KEncoder(Encoder):
         else:
             yuv_img = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
         return yuv_img
-    
+
     @staticmethod
     def discrete_wavelet_transform_2d(image: np.ndarray):
         """
@@ -123,7 +144,7 @@ class JP2KEncoder(Encoder):
         frequency = Counter(data)
         heap = [[weight, [symbol, ""]] for symbol, weight in frequency.items()]
         heapq.heapify(heap)
-        
+
         while len(heap) > 1:
             lo = heapq.heappop(heap)
             hi = heapq.heappop(heap)
@@ -132,15 +153,16 @@ class JP2KEncoder(Encoder):
             for pair in hi[1:]:
                 pair[1] = '1' + pair[1]
             heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
-        
-        huffman_tree = sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+
+        huffman_tree = sorted(heapq.heappop(
+            heap)[1:], key=lambda p: (len(p[-1]), p))
         huffman_dict = {symbol: code for symbol, code in huffman_tree}
         encoded_data = "".join(huffman_dict[symbol] for symbol in data)
-        
+
         return encoded_data, huffman_tree
 
     @staticmethod
-    def serialize_huffman_tree(huffman_tree:Dict) -> List[Tuple[int,str]]:
+    def serialize_huffman_tree(huffman_tree: Dict) -> List[Tuple[int, str]]:
         """
         序列化霍夫曼树以便存储
         :param huffman_tree: 霍夫曼树
@@ -150,7 +172,7 @@ class JP2KEncoder(Encoder):
         for symbol, code in huffman_tree:
             serialized_tree.append((symbol, code))
         return serialized_tree
-    
+
     @staticmethod
     def process_block(block: np.ndarray, q_factor: int) -> Tuple[str, Dict]:
         """
@@ -162,12 +184,13 @@ class JP2KEncoder(Encoder):
         coeffs = JP2KEncoder.discrete_wavelet_transform_2d(block)
         cA, (cH, cV, cD) = JP2KEncoder.quantize(coeffs, q_factor)
         # 将量化后的系数转成一行数组
-        flat_array = np.hstack((cA.flatten(), cH.flatten(), cV.flatten(), cD.flatten())).astype(int)
+        flat_array = np.hstack(
+            (cA.flatten(), cH.flatten(), cV.flatten(), cD.flatten())).astype(int)
         encoded_data, huffman_tree = JP2KEncoder.huffman_encoding(flat_array)
         return encoded_data, huffman_tree
 
     @staticmethod
-    def organize_bitstream(blocks: List[np.ndarray], q_factor:int) -> Tuple[ByteString,Dict[int,str]]:
+    def organize_bitstream(blocks: List[np.ndarray], q_factor: int) -> Tuple[ByteString, Dict[int, str]]:
         """
         组织码流
         :param blocks: 图像块列表
@@ -181,10 +204,12 @@ class JP2KEncoder(Encoder):
             # 对每个通道进行处理
             for channel in range(block.shape[2]):
                 # 对数据进行小波变换、量化和熵编码
-                encoded_data, huffman_tree = JP2KEncoder.process_block(block[:,:,channel], q_factor)
+                encoded_data, huffman_tree = JP2KEncoder.process_block(
+                    block[:, :, channel], q_factor)
                 huffman_trees.append(huffman_tree)
                 # 序列化霍夫曼树
-                serialized_tree = JP2KEncoder.serialize_huffman_tree(huffman_tree)
+                serialized_tree = JP2KEncoder.serialize_huffman_tree(
+                    huffman_tree)
 
                 # 将编码后的数据写入码流
                 bitstream += struct.pack('I', len(encoded_data))
