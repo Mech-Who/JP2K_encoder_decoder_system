12b1e10b5efbcee33155f3bc9fab27224688e2a0
hushuhan <873933169@qq.com>
2024-06-10 20:59:02 +0800

    main: Added 添加小波图像保存路径，为方案一（先保存小波图，再显示）准备

--- src/config_/path_config.py
+++ src/config_/path_config.py
@@ -6,5 +6,6 @@ from pathlib import Path
 # ENCODED_IMG_ROOT = Path('/AIstationzyy/IC/JP2K/img/encoded_img')
 # windows
 SRC_IMG_ROOT = Path(r'C:\UserData\Projects\MyProjects\JP2K_System\img\src_img')
-DECODED_IMG_ROOT = Path(r'C:\UserData\Projects\MyProjects\JP2K_System\img\decoded_img')
+WAVE_IMG_ROOT = Path(r'C:\UserData\Projects\MyProjects\JP2K_System\img\wave_img')
 ENCODED_IMG_ROOT = Path(r'C:\UserData\Projects\MyProjects\JP2K_System\img\encoded_img')
+DECODED_IMG_ROOT = Path(r'C:\UserData\Projects\MyProjects\JP2K_System\img\decoded_img')
